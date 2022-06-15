e7d95f56a062fa795fb369a3441c7a0c2c3dc3f5
Collins Mundia <collins.mwangi@student.moringaschool.com>
2022-06-15 17:21:17 +0300

    commit tree f8b899daab199db3ec79e59c26334dac78c50c15

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+ function scuberGreetingForFeet(ride){
   // Write your code here!
+    if (ride <= 400) {
+      return 'This one is on me!';
+    } else if (1000 <= ride && ride <= 2500) {
+      return "I will gladly take your thirty bucks.";
+    } else {
+      return "No can do.";
+    }
+  }
+
+console.log(scuberGreetingForFeet(400));
+
+function ternaryCheckCity(city){
+  // Write your code here!
+   return (city==='NYC')?'Ok, sounds good.':'No go.'
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
+  switch(tip){
+    case 'generous':
+    return 'Thank you so much.';
+    break;
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+    case 'not as generous':
+    return "Thank you."
+
+    default:
+      return "Bye."
+  }
 }
+// console.log(switchOnCharmFromTip());
